6ea642490908e4e2faf3ba50f76c687b1dc35555
isa851 <isa904363@gmail.com>
2025-05-15 19:07:30 +0300

    qr

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Input, QRCode, Space } from "antd";
-import "./app.scss"
+import "./app.scss";
+
 function App() {
-  const [text, setText] = React.useState("ссылку поставь");
+  const [text, setText] = React.useState("Введите ссылку!");
+
   return (
-    <>
+    <div className="qr-container">
+      <h1>QR Code Generator</h1>
       <Space direction="vertical" align="center">
-        <QRCode value={text || "-"} />
+        <QRCode value={text || "-"} size={200} />
         <Input
-          placeholder="-"
+          placeholder="Enter your link"
           maxLength={60}
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
       </Space>
-    </>
+    </div>
   );
 }
 
